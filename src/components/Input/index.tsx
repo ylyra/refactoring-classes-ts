f0ff4730b8ddab36e4fbec70f4a852a8711d7f1d
Yan Lyra <yanlyra3@gmail.com>
2021-08-24 19:53:23 -0300

    ✨ Finished challenge

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,18 @@
-import {
-  useEffect,
-  useRef,
-  useState,
-  useCallback,
-} from 'react';
+import { useEffect, useRef, useState, useCallback } from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface Props {
+  name: string;
+  icon?: any;
+}
+
+type InputProps = JSX.IntrinsicElements["input"] & Props;
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -31,7 +33,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+
+import Header from "../../components/Header";
+import api from "../../services/api";
+import Food from "../../components/Food";
+import ModalAddFood from "../../components/ModalAddFood";
+import ModalEditFood from "../../components/ModalEditFood";
+import { FoodsContainer } from "./styles";
+
+interface FoodProps {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
+
+function Dashboard() {
+  const [foods, setFoods] = useState<FoodProps[]>([]);
+  const [editingFood, setEditingFood] = useState({} as FoodProps);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    async function loadFoods() {
+      try {
+        const response = await api.get<FoodProps[]>("/foods");
+        setFoods(response.data);
+      } catch (error) {}
+    }
+    loadFoods();
+  }, []);
+
+  const handleAddFood = async (food: FoodProps) => {
+    try {
+      const response = await api.post<FoodProps>("/foods", {
+        ...food,
+        available: true,
+      });
+
+      setFoods((oldVersion) => [...oldVersion, response.data]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdateFood = async (food: FoodProps) => {
+    try {
+      const foodUpdated = await api.put(`/foods/${editingFood.id}`, {
+        ...editingFood,
+        ...food,
+      });
+
+      const foodsUpdated = foods.map((f) =>
+        f.id !== foodUpdated.data.id ? f : foodUpdated.data
+      );
+
+      setFoods(foodsUpdated);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDeleteFood = async (id: number) => {
+    await api.delete(`/foods/${id}`);
+
+    const foodsFiltered = foods.filter((food) => food.id !== id);
+    setFoods(foodsFiltered);
+  };
+
+  const toggleModal = () => {
+    setModalOpen((oldVersion) => !oldVersion);
+  };
+
+  const toggleEditModal = () => {
+    setEditModalOpen((oldVersion) => !oldVersion);
+  };
+
+  const handleEditFood = (food: FoodProps) => {
+    setEditingFood(food);
+    setEditModalOpen(true);
+  };
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map((food) => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
+
+export default Dashboard;
